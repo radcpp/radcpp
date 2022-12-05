c02d2142bd58080ad8ef9c8db0e72bee9620c65a
Ma, Jerry <majunlichn@outlook.com>
2022-12-06 01:29:25 +0800

    rad/Core/Containers

---
+++ include/rad/Core/Containers.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "rad/Core/Common.h"
+
+// Sequence containers
+#include <array>
+#include <vector>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include "rad/Core/SmallVector.h"
+
+// Associative containers
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+// Adapters
+#include <stack>
+#include <queue>
+#include <span>
+
+#include "rad/Core/Span.h"
